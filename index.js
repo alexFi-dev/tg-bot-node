75fbb76908943e2d94ff0aae19a5620dbef1d253
Alexeev Vadim <alexeev.vadim@challenge.day>
2023-10-16 12:42:58 +0700

    update 16 Oct.
    - added IONIC version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '6294915141:AAFmbmw2KulPy-qd3MOjTLtYu4u8fK8k9OM';
 const webAppUrlOne = 'https://luminous-quokka-ea18d1.netlify.app/';
 const webAppUrlTwo = 'https://chimerical-stroopwafel-0fd3e2.netlify.app/';
+const webAppUrlThree = 'https://tiny-cactus-69a5da.netlify.app/';
 
 const bot = new TelegramBot(token, {polling: true});
 
@@ -14,7 +15,7 @@ bot.on('message', async (msg) => {
     await bot.sendMessage(chatId, '💱 Добро пожаловать в виртуальный обменник! 💵  Настоящая версия: MVP v.0.5 (beta 0.1). Доступен архив:', {
       reply_markup: {
         inline_keyboard: [
-          [{text: 'prototype 💸', web_app: {url: webAppUrlOne}}, {text: 'alpha v.0.1 💰', web_app: {url: webAppUrlTwo}}]
+          [[{text: 'prototype 💸', web_app: {url: webAppUrlOne}}, {text: 'alpha v.0.1 💰', web_app: {url: webAppUrlTwo}}],[{text: 'IONIX UX+UI 📱', web_app: {url: webAppUrlThree}}]]
         ]
       }
     });
